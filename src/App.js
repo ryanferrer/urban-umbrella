f38f198358336778558fac0eecaa632069c5d5c3
Ryan <ryferr8122@gmail.com>
2025-10-13 01:14:22 -0500

    Can map over array of objects

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import ygodata from './data/ygojson.json';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        ygodata.map((card) => {
+          if (card["Total Quantity"] > 0) {
+            return (
+              <div>
+                <p>
+                {card["Total Quantity"]}
+                </p>
+
+                <p>
+                  {card["Product Name"]}
+                </p>
+
+                <p>
+                  {card["TCG Market Price"]}
+                </p>
+              </div>
+            )
+          }
+        })
+      }
     </div>
   );
 }
